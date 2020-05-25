c1432698c6b83c399903cfcabd3f52053f4ce0c5
michael20996 <michael20996@gmail.com>
2020-05-24 20:26:24 -0700

    Created login and registration

---
+++ client/build/precache-manifest.90f5ea847f2cdac8adf81039072d3c3a.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "a431e0214f206724a8fc",
+    "url": "/static/css/main.f24857a7.chunk.css"
+  },
+  {
+    "revision": "a431e0214f206724a8fc",
+    "url": "/static/js/main.3e7a9e86.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  },
+  {
+    "revision": "67b02862788f67ecd296",
+    "url": "/static/js/2.60d56000.chunk.js"
+  },
+  {
+    "revision": "0f1a2327f93961763e7be717c425f9b2",
+    "url": "/static/media/babylogo.0f1a2327.png"
+  },
+  {
+    "revision": "b2a8e574cd321d04973642437bd2c4f1",
+    "url": "/index.html"
+  }
+];
