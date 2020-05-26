10ac2d82b6bce8eefdf1b5a0e911f66db61afb44
ar <56378858+arumbaua366@users.noreply.github.com>
2020-05-25 17:25:01 -0700

    update readme, add .env and gitignore files, start creating profile page

--- README.md
+++ README.md
@@ -1,1 +1,18 @@
-# happy-nappers
+# Project Title
+Happy Nappers
+
+# User Story
+
+
+# Technology Used
+
+
+# Authors
+[Mark Ohanasian](https://github.com/markohanesian) 
+[Andrea Rumbaua](https://github.com/arumbaua366)
+[Don Nguyen](https://github.com/TDPN)
+[Michael Le](https://github.com/michael20996)
+
+
+# Contributors
+Icons made by [Eucalyp](https://www.flaticon.com/authors/eucalyp) from [www.flaticon.com](https://www.flaticon.com)
--- server.js
+++ server.js
@@ -4,6 +4,7 @@ const mongoose = require("mongoose");
 const routes = require("./routes");
 const PORT = process.env.PORT || 3001;
 const app = express();
+const SpotifyStrategy = require("passport-spotify").Strategy
 app.use (express.urlencoded({extended: true}))
 app.use (express.json())
 
@@ -14,6 +15,22 @@ if (process.env.NODE_ENV === "production") {
 
 // Add routes, both API and view
 app.use(routes);
+
+// Passport Spotify routes
+passport.use(
+  new SpotifyStrategry(
+    {
+      clientID: client_id,
+      clientSecret: client_secret,
+      callbackURL: "http://localhost:8888/auth/spotify/callback"
+    },
+    function(accessToken, refreshToken, expires_in, profile, done) {
+      User.findOrCreate({ spotifyId: profile.id }, function(err, user) {
+        return done(err, user);
+      });
+    }
+  )
+)
 
 // Connect to the Mongo DB
 mongoose.connect(process.env.MONGODB_URI || "mongodb://localhost/happer-nappers");
