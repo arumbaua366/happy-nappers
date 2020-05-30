fadd23e1a2908316584691987dd1fd2dfd733b70
ar <56378858+arumbaua366@users.noreply.github.com>
2020-05-30 13:13:38 -0700

    updates

--- src/components/spotify-player-one.js
+++ src/components/spotify-player-one.js
@@ -1,8 +1,17 @@
 var SpotifyWebApi = require('spotify-web-api-node');
 
+
 // credentials are optional
 var spotifyApi = new SpotifyWebApi({
   clientId: 'fcecfc72172e4cd267473117a17cbd4d',
   clientSecret: 'a6338157c9bb5ac9c71924cb2940e1a7',
-  redirectUri: 'http://www.example.com/callback'
+  redirectUri: 'http://localhost:3000/spotify/callback'
 });
+
+// Search playlists whose name or description contains 'workout'
+spotifyApi.searchPlaylists('workout')
+  .then(function(data) {
+    console.log('Found playlists are', data.body);
+  }, function(err) {
+    console.log('Something went wrong!', err);
+  });
