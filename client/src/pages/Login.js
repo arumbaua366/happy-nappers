7b874a128b1917d53891e20571ec53b146261441
ar <56378858+arumbaua366@users.noreply.github.com>
2020-06-04 19:31:26 -0700

    pull updates'

--- client/src/pages/Login.js
+++ client/src/pages/Login.js
@@ -4,13 +4,9 @@ import GridLogin from "../components/GridLogin"
 function Login() {
     return (
         <div className='App'>
-<<<<<<< HEAD
-          <a href='/spotify/login'> Login to Spotify </a>
-=======
                         <GridLogin />
 
           <a href='http://localhost:8888'> Login to Spotify </a>
->>>>>>> 8eaa9b8fe185f16eb3dc0639350dbae9176fdb9d
         </div>
       )
 }
