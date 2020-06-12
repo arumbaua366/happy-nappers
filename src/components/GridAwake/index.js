b4bd1bd616aab6d3b46ef1730a7e0262122b94d5
Don <nguyentuand85@gmail.com>
2020-06-11 19:09:10 -0700

    adding spotify player

--- src/components/GridAwake/index.js
+++ src/components/GridAwake/index.js
@@ -1,8 +1,17 @@
-import React from 'react';
-import { makeStyles } from '@material-ui/core/styles';
-import Paper from '@material-ui/core/Paper';
-import Grid from '@material-ui/core/Grid';
+import React from "react";
+import { makeStyles } from "@material-ui/core/styles";
+import Paper from "@material-ui/core/Paper";
+import Grid from "@material-ui/core/Grid";
 // import DeleteBtn from "../DeleteBtn"
+import SpotifyPlayer from "react-spotify-player";
+
+// size may also be a plain string using the presets 'large' or 'compact'
+const size = {
+  width: "100%",
+  height: 300,
+};
+const view = "list"; // or 'coverart'
+const theme = "white"; // or 'black'
 
 const useStyles = makeStyles((theme) => ({
   root: {
@@ -10,9 +19,9 @@ const useStyles = makeStyles((theme) => ({
   },
   paper: {
     padding: theme.spacing(2),
-    textAlign: 'center',
+    textAlign: "center",
     color: theme.palette.text.secondary,
-    alignItems: 'center'
+    alignItems: "center",
   },
 }));
 
@@ -24,19 +33,32 @@ export default function CenteredGrid() {
       <Grid container spacing={0}>
         <Grid item xs={12}>
           <Paper className={classes.paper}>
-              <img src="https://media3.giphy.com/media/qZohEEh4bhuQ8/giphy.gif?cid=6104955eec03a397e8f246036534b4597eed2d1fcfa22281&rid=giphy.gif" alt="Sun" style={{verticalAlign: "middle", width: "500px", height: "500px", padding: "30px" }}></img>
-            </Paper>
+            <img
+              src="https://media3.giphy.com/media/qZohEEh4bhuQ8/giphy.gif?cid=6104955eec03a397e8f246036534b4597eed2d1fcfa22281&rid=giphy.gif"
+              alt="Sun"
+              style={{
+                verticalAlign: "middle",
+                width: "500px",
+                height: "500px",
+                padding: "30px",
+              }}
+            ></img>
+          </Paper>
         </Grid>
         {/* <Grid item xs={12}>
           <Paper className={classes.paper}>
           </Paper>
         </Grid> */}
         <Grid item xs={12}>
-          <Paper className={classes.paper}>
-
-          </Paper>
+          <Paper className={classes.paper}></Paper>
         </Grid>
       </Grid>
+      <SpotifyPlayer
+        uri="spotify:album:1TIUsv8qmYLpBEhvmBmyBk"
+        size={size}
+        view={view}
+        theme={theme}
+      />
     </div>
   );
 }
