c675ea2ba88538e3e5bb3aacbf8555572a1cc47b
Mark <mso872@gmail.com>
2020-06-13 10:35:09 -0700

    updated awake

--- src/components/GridAwake/index.js
+++ src/components/GridAwake/index.js
@@ -32,22 +32,12 @@ export default function CenteredGrid() {
       <Grid container spacing={0}>
         <Grid item xs={12}>
           <Paper className={classes.paper}>
-<<<<<<< HEAD
-            <img
-              src="https://media3.giphy.com/media/qZohEEh4bhuQ8/giphy.gif?cid=6104955eec03a397e8f246036534b4597eed2d1fcfa22281&rid=giphy.gif"
-              alt="Sun"
-              style={{
-                verticalAlign: "middle",
-                maxWidth: "700px",
-                padding: "0px",
-                display: "center",
-              }}
-            ></img>
+              <img src="https://media1.tenor.com/images/714043687bc499a2c53f00af4390292a/tenor.gif?itemid=15577102" alt="Sun" 
+              style={{verticalAlign: "middle",
+                    maxWidth: "300px",
+                    padding: "0px",
+                    display: "center", }}></img>
           </Paper>
-=======
-              <img src="https://media1.tenor.com/images/714043687bc499a2c53f00af4390292a/tenor.gif?itemid=15577102" alt="Sun" style={{verticalAlign: "middle", width: "500px", height: "500px", padding: "30px" }}></img>
-            </Paper>
->>>>>>> 3abc991bad65e25ec72ca29398907f0d2b36e8b4
         </Grid>
         {/* <Grid item xs={12}>
           <Paper className={classes.paper}>
@@ -58,7 +48,7 @@ export default function CenteredGrid() {
 
           </Paper>
         </Grid> */}
-      </Grid>
+    </Grid>
       <SpotifyPlayer
         uri="spotify:album:4FNX7VLAhDVWn6ghmaHFTp"
         size={size}
