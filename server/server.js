28871292bd0c58967661b89bdf1c85befe18f257
ar <56378858+arumbaua366@users.noreply.github.com>
2020-06-16 21:22:34 -0700

    updated server

--- server/server.js
+++ server/server.js
@@ -8,6 +8,10 @@ const passport = require('./passport');
 const app = express()
 const PORT = process.env.PORT || 3001;
 // Route requires
+// Serve up static assets (usually on heroku)
+if (process.env.NODE_ENV === "production") {
+  app.use(express.static("client/build"));
+}
 const user = require('./routes/user')
 
 
@@ -37,6 +41,11 @@ app.use(passport.session()) // calls the deserializeUser
 
 // Routes
 app.use('/user', user)
+// Send every request to the React app
+// Define any API routes before this runs
+app.get("*", function(req, res) {
+	res.sendFile("../build/index.html");
+  });
 // app.use('/', express.static('../build'))
 // Starting Server 
 app.listen(PORT, () => {
