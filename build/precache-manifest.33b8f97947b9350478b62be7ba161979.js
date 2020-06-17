1759971c5f24fc039462af4a5d272de77e386f35
ar <56378858+arumbaua366@users.noreply.github.com>
2020-06-16 19:37:53 -0700

    update heroku buildpacks

---
+++ build/precache-manifest.33b8f97947b9350478b62be7ba161979.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c718bf68ad04060ac37af2a3e260d1a0",
+    "url": "/index.html"
+  },
+  {
+    "revision": "832bf502bdd58c925022",
+    "url": "/static/css/2.0953b03e.chunk.css"
+  },
+  {
+    "revision": "59ae87310110ee6d4d8b",
+    "url": "/static/css/main.2d080ac8.chunk.css"
+  },
+  {
+    "revision": "832bf502bdd58c925022",
+    "url": "/static/js/2.5f1bf5c1.chunk.js"
+  },
+  {
+    "revision": "d4f785a5ebd441c086da94f1709dc4d0",
+    "url": "/static/js/2.5f1bf5c1.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "59ae87310110ee6d4d8b",
+    "url": "/static/js/main.82bf84fa.chunk.js"
+  },
+  {
+    "revision": "2af78c11134a623570de",
+    "url": "/static/js/runtime-main.54b3378c.js"
+  }
+]);
