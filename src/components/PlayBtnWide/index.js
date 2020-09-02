efa2a3a0c9198624d5dc5d1c7ba64780cb2721e3
Mark <mso872@gmail.com>
2020-09-02 14:21:24 -0700

    updated button styling

---
+++ src/components/PlayBtnWide/index.js
@@ -0,0 +1,25 @@
+import React from 'react';
+import { makeStyles } from '@material-ui/core/styles';
+import Button from '@material-ui/core/Button';
+import "./AwakeBtnWide.css";
+
+const useStyles = makeStyles((theme) => ({
+  root: {
+    '& > *': {
+      // margin: theme.spacing(0),
+      // fullWidth: true,
+    },
+  },
+}));
+
+export default function AwakeButton() {
+  const classes = useStyles();
+
+  return (
+    <div className={classes.root}>
+      <Button id="awake-btn" href="/awake" variant="light">
+        play
+      </Button>
+    </div>
+  );
+}
